0a76af77d21e4e2420219eff3d116bbf73da5131
Walid Hocine <30287336+hocinewalid@users.noreply.github.com>
2018-04-04 14:35:10 +0200

    insertion des utilisateurs

---
+++ 4 insertion des utilisateurs.sql
@@ -0,0 +1,7 @@
+INSERT INTO utilisateurs (pseudo, motDePasse, statusAdmin)
+VALUES
+('Katere', 'toppot','0'),
+('Chap', 'elgnuj','0'),
+('Melon', 'middim','0'),
+('Wakz', 'adda','1'),
+('Caëlan', 'troppus','0');
