7e2a927d333a683f1c77da5ffcb6423d26aa614c
Walid Hocine <30287336+hocinewalid@users.noreply.github.com>
2018-04-04 14:24:19 +0200

    Mise a jour du script php d'inscription
    verification de la disponibilité d'un nom d'utilisateur

--- scriptInscription.php
+++ scriptInscription.php
@@ -1,36 +1,20 @@
 <?php
-
-
+$bdd = new PDO('mysql:host=localhost;dbname=wsprosit5;charset=utf8', 'root', '');
 
 $pseudo = $_POST['pseudo'];
 $motDePasse = $_POST['motDePasse'];
 
-$bdd = new PDO( 'mysql:host=localhost;dbname=wsprosit5;charset=utf8','root','');
-try
-{
-$requete = $bdd->prepare("INSERT INTO utilisateurs (pseudo, motDePasse) VALUES ( :pseudo, :motDePasse)");
+$req = $bdd->prepare('SELECT id FROM utilisateurs WHERE pseudo=?');
+$req->execute(array($_POST['pseudo']));
+
+if ($donnees = $req->fetch()) {
+    echo "this username is already used by another user";
+} else {
+    // le pseudo n'est pas utilisé
+    echo "this username is availble";
+    // Requête préparée pour empêcher les injections SQL
+    $requete = $bdd->prepare("INSERT INTO utilisateurs (pseudo, motDePasse) VALUES( :pseudo,:motDePasse)");
+    $requete->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+$requete->bindValue(':motDePasse', $motDePasse, PDO::PARAM_STR);
+$requete->execute();
 }
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
-
-$result = $requete->fetch();
-if($result)
-{
-if($result[0] == 1) echo "Username already taken";
-else "username free";
-}
-
-$requete->bindValue(' :pseudo',$pseudo,PDO::PARAM_STR);
-$requete->bindValue(' :motDePasse',$motDePasse,PDO::PARAM_STR);
-
-$requete->execute(array(
-	'pseudo' => $pseudo,
-	'motDePasse'=>$motDePasse,
-
-
-
-));
-
-?>
