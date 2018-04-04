2e2ebfa5942484c5e88bbf452cecd20bebcdd6e3
Walid Hocine <30287336+hocinewalid@users.noreply.github.com>
2018-04-04 14:31:41 +0200

    Delete WSpart4.sql

--- WSpart4.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE utilisateurs (
-    ID int NOT NULL AUTO_INCREMENT,
-    pseudo varchar (15) NOT NULL,
-    motDePasse varchar(10),
-    StatusAdmin BOOLEAN DEFAULT 0,
-    PRIMARY KEY (ID)
-);
