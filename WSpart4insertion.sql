0b6c4978145f4f8b45bb3744fc4d4da9dac70c7b
Walid Hocine <30287336+hocinewalid@users.noreply.github.com>
2018-04-04 14:33:40 +0200

    Delete WSpart4insertion.sql

--- WSpart4insertion.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO utilisateurs (pseudo, motDePasse, statusAdmin)
-VALUES
-('Katere', 'toppot','0'),
-('Chap', 'elgnuj','0'),
-('Melon', 'middim','0'),
-('Wakz', 'adda','1'),
-('Caëlan', 'troppus','0');
